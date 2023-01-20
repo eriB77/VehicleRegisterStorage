1efa8391e5cbdcb94ceac1d5bb7df82ba48eecdf
Erika Birtha <ebirtha@hotmail.com>
2023-01-20 17:52:24 +0100

    implement Filestorage

--- src/main/java/hu/erikabirtha/FileStorage.java
+++ src/main/java/hu/erikabirtha/FileStorage.java
@@ -1,2 +1,17 @@
-package hu.erikabirtha;public class FileStorage {
+package hu.erikabirtha;
+
+import hu.erikabirtha.entity.Vehicle;
+
+public class FileStorage implements VehiclePersistence {
+
+    @Override
+    public void saveVehicle(Vehicle vehicle) {
+        System.out.println("File mentés!");
+
+    }
+
+    @Override
+    public Vehicle loadVehicle(String s) {
+        return null;
+    }
 }
